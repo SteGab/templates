8a2933c4ae5074574ccaf06b7cc4fb5c0849dcc6
STG <gabauer@media-solutions.de>
2025-03-20 16:22:06 +0100

    Angular Template added

---
+++ Devcontainer/Angular_dc/.devcontainer/setup.sh
@@ -0,0 +1,5 @@
+# From: https://dev.to/srinivasarajui/angular-development-on-vs-code-and-docker-using-remote-containers-5a1a
+npm install -g @angular/cli
+
+#apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get -y install --no-install-recommends chromium
+#export CHROME_BIN=/usr/bin/chromium
