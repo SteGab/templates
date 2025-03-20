33f88f225f1cf7574ed50a5a204d68495ec1f4a7
STG <gabauer@media-solutions.de>
2025-03-20 16:51:13 +0100

    Setup script

--- Devcontainer/Angular_dc/.devcontainer/setup.sh
+++ Devcontainer/Angular_dc/.devcontainer/setup.sh
@@ -1,5 +1,6 @@
 # From: https://dev.to/srinivasarajui/angular-development-on-vs-code-and-docker-using-remote-containers-5a1a
 npm install -g @angular/cli
 
+#ng new AngularOnRC --directory .
 #apt-get update && export DEBIAN_FRONTEND=noninteractive && apt-get -y install --no-install-recommends chromium
 #export CHROME_BIN=/usr/bin/chromium
